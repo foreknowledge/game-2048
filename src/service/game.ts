b42433aa68651da37bf25b7284cab12b785628c5
foreknowledge <kyj_0129@naver.com>
2022-07-09 18:11:44 +0900

    feat: 점수 계산 로직 추가

--- src/service/game.ts
+++ src/service/game.ts
@@ -2,6 +2,7 @@ import Field from './field';
 
 export default class Game {
   field: Field;
+  totScore = 0;
 
   constructor(size: number) {
     this.field = new Field(size);
@@ -14,6 +15,7 @@ export default class Game {
   move(direction: 'U' | 'D' | 'L' | 'R') {
     // 한 턴이 끝나면 merge logs 초기화
     this.field.clearMergeLogs();
+
     const before = this.field.clone();
 
     switch (direction) {
@@ -31,11 +33,9 @@ export default class Game {
         break;
     }
 
-    // 현재 필드에 있는 모든 카드에 대해 merge log 조회
-    this.field.getAllCards().forEach((card) => {
-      const mergeLog = this.field.getMergeLog(card.id);
-      mergeLog && console.log(mergeLog.cardIds);
-    });
+    // 점수 계산
+    const score = this.calcScore();
+    score && (this.totScore += score);
 
     if (this.field.isFull()) {
       // 게임 필드가 꽉 찼으면 게임 종료!
@@ -48,5 +48,16 @@ export default class Game {
     }
 
     this.field.printMap();
+  }
+
+  calcScore(): number {
+    return this.field
+      .getAllCards()
+      .map((card) => {
+        const mergeLog = this.field.getMergeLog(card.id);
+        if (mergeLog) return card.num;
+        else return 0;
+      })
+      .reduce((sum, current) => sum + current);
   }
 }
