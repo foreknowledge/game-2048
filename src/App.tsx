8482d9e1d05a5c890bbe5faba3ede1761ec84b2d
foreknowledge <kyj_0129@naver.com>
2022-07-10 14:40:27 +0900

    feat(UI): Add tiles at game field

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,13 @@
-import { useEffect } from 'react';
 import styles from './App.module.css';
 import GameField from './components/game_field/game_field';
 import Header from './components/header/header';
 import Game from './service/game';
 
 function App({ game }: { game: Game }) {
-  const handleKeyDown = (e: KeyboardEvent) => {
-    switch (e.key) {
-      case 'ArrowUp':
-        console.log(game.move('U'));
-        break;
-      case 'ArrowDown':
-        console.log(game.move('D'));
-        break;
-      case 'ArrowLeft':
-        console.log(game.move('L'));
-        break;
-      case 'ArrowRight':
-        console.log(game.move('R'));
-        break;
-    }
-  };
-  useEffect(() => {
-    window.addEventListener('keydown', handleKeyDown);
-
-    // cleanup this component
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, []);
-
   return (
     <div className={styles.container}>
       <Header score={0} best={0} />
-      <GameField />
+      <GameField game={game} />
       <div className={styles.footer}>
         <button className={styles.btn_reset}>RESET</button>
       </div>
--- src/components/game_field/game_field.tsx
+++ src/components/game_field/game_field.tsx
@@ -1,9 +1,41 @@
+import { useEffect } from 'react';
+import Game from '../../service/game';
 import styles from './game_field.module.css';
 
-const GameField = () => {
+const GameField = ({ game }: { game: Game }) => {
+  const handleKeyDown = (e: KeyboardEvent) => {
+    switch (e.key) {
+      case 'ArrowUp':
+        console.log(game.move('U'));
+        break;
+      case 'ArrowDown':
+        console.log(game.move('D'));
+        break;
+      case 'ArrowLeft':
+        console.log(game.move('L'));
+        break;
+      case 'ArrowRight':
+        console.log(game.move('R'));
+        break;
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+
+    // cleanup this component
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   return (
     <section className={styles.gameField}>
-      <div className={styles.gameGrid}></div>
+      <div className={styles.gameGrid}>
+        {[...Array(game.field.size * game.field.size).keys()].map((i) => (
+          <div key={i} className={styles.gameTile} />
+        ))}
+      </div>
     </section>
   );
 };
