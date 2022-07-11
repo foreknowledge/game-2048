9f5ee8ebdb024b25d9739254735016f86bd932f6
foreknowledge <kyj_0129@naver.com>
2022-07-11 23:39:26 +0900

    feat: tileSize 구하기

--- src/components/game_field/game_field.tsx
+++ src/components/game_field/game_field.tsx
@@ -1,10 +1,22 @@
+import { useLayoutEffect, useRef, useState } from 'react';
 import Field from '../../service/field';
 import styles from './game_field.module.css';
 
 const GameField = ({ field }: { field: Field }) => {
+  const [tileSize, setTileSize] = useState(0);
+  const gridRef = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    // tile size가 결정된 후에 카드를 그려야 하므로 useLayoutEffect 안에서 tile size를 설정한다.
+    const newTileSize =
+      gridRef.current?.children[0].getBoundingClientRect().width ?? 0;
+    setTileSize(newTileSize);
+  });
+
   return (
     <section className={styles.gameField}>
       <div
+        ref={gridRef}
         className={styles.gameGrid}
         style={{ gridTemplateColumns: `repeat(${field.size} ,1fr)` }}
       >
