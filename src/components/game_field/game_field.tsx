31aab0361ab0c2719e0d013792209b8e411455d3
foreknowledge <kyj_0129@naver.com>
2022-07-12 00:09:31 +0900

    feat: 카드에 스타일 적용

--- src/components/game_field/game_field.tsx
+++ src/components/game_field/game_field.tsx
@@ -53,7 +53,7 @@ const GameField = ({ field }: { field: Field }) => {
         return (
           <div
             key={uiCard.id}
-            className={styles.card}
+            className={`${styles.card} ${getCardStyle(uiCard.num)}`}
             style={{
               top: uiCard.top,
               left: uiCard.left,
@@ -70,3 +70,28 @@ const GameField = ({ field }: { field: Field }) => {
 };
 
 export default GameField;
+
+function getCardStyle(num: number): string {
+  switch (num) {
+    case 2:
+      return styles.level1;
+    case 4:
+      return styles.level2;
+    case 8:
+      return styles.level3;
+    case 16:
+      return styles.level4;
+    case 32:
+      return styles.level5;
+    case 64:
+      return styles.level6;
+    case 128:
+      return styles.level7;
+    case 256:
+      return styles.level8;
+    case 512:
+      return styles.level9;
+    default:
+      return styles.level10;
+  }
+}
