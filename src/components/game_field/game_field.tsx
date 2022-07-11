1e9b06f1ebec6a69ad2332e5ff5381901b89841c
foreknowledge <kyj_0129@naver.com>
2022-07-11 23:51:50 +0900

    feat: Grid UI에 카드 표시

--- src/components/game_field/game_field.tsx
+++ src/components/game_field/game_field.tsx
@@ -1,10 +1,35 @@
-import { useLayoutEffect, useRef, useState } from 'react';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import Field from '../../service/field';
 import styles from './game_field.module.css';
 
+type UICard = {
+  top: number;
+  left: number;
+  id: number;
+  num: number;
+};
+
 const GameField = ({ field }: { field: Field }) => {
   const [tileSize, setTileSize] = useState(0);
+  const [uiCards, setUICards] = useState<UICard[]>([]);
   const gridRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!gridRef.current) return;
+
+    const grid = gridRef.current;
+    const newUICards = field.getAllCards().map((card) => {
+      const { row, col } = field.getCardPos(card.id)!;
+      const tile = grid.children[row * field.size + col] as HTMLElement;
+      return {
+        top: tile.offsetTop,
+        left: tile.offsetLeft,
+        id: card.id,
+        num: card.num,
+      };
+    });
+    setUICards(newUICards);
+  }, [gridRef.current, field]);
 
   useLayoutEffect(() => {
     // tile size가 결정된 후에 카드를 그려야 하므로 useLayoutEffect 안에서 tile size를 설정한다.
@@ -24,6 +49,22 @@ const GameField = ({ field }: { field: Field }) => {
           <div key={i} className={styles.gameTile} />
         ))}
       </div>
+      {uiCards.map((uiCard) => {
+        return (
+          <div
+            key={uiCard.id}
+            className={styles.card}
+            style={{
+              top: uiCard.top,
+              left: uiCard.left,
+              width: tileSize,
+              height: tileSize,
+            }}
+          >
+            {uiCard.num}
+          </div>
+        );
+      })}
     </section>
   );
 };
