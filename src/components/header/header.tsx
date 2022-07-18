a0beeada3ac503554c85ceb6329cff942e1eb31a
foreknowledge <kyj_0129@naver.com>
2022-07-18 23:18:42 +0900

    refactor: use memo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,19 +1,20 @@
+import { memo } from 'react';
 import styles from './header.module.css';
 
-const Header = (props: { score: number; best: number }) => (
+const Header = memo(({ score, best }: { score: number; best: number }) => (
   <>
     <h1 className={styles.title}>2048</h1>
     <div className={styles.scores}>
       <div className={styles.scoreBox}>
         <div className={styles.scoreTitle}>SCORE</div>
-        <div className={styles.scoreValue}>{props.score}</div>
+        <div className={styles.scoreValue}>{score}</div>
       </div>
       <div className={styles.scoreBox}>
         <div className={styles.scoreTitle}>BEST</div>
-        <div className={styles.scoreValue}>{props.best}</div>
+        <div className={styles.scoreValue}>{best}</div>
       </div>
     </div>
   </>
-);
+));
 
 export default Header;
